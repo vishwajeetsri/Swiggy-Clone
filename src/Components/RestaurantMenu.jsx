f8b10dd837d3a380b703e28cc68dcc41cda5e966
vishwajeetsri <vs0397297@gmail.com>
2025-08-08 23:28:55 +0530

    Fifth Commit

--- src/Components/RestaurantMenu.jsx
+++ src/Components/RestaurantMenu.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const RestaurantMenu = () => {
+  const [resData, setResData] = useState(null);
 
-    const [resData, setResData] = useState(null)
+  let { id } = useParams();
+  console.log(id);
+
+  const fetchData = async () => {
+    const res = await fetch(
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5929605&lng=77.2972601&restaurantId=${id}`
+    );
+    const data = await res.json();
+    
+ const tempData = data?.data?.cards?.find(card => card?.groupedCard)?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+   const filterData = Array.isArray(tempData)
+  ? tempData.filter((items) => "title" in items?.card?.card)
+  : [];
   
-    let {id} = useParams()
-    console.log(id);
+    setResData(filterData);
+  };
 
-    const fetchData = async () => {
-        const res = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5929605&lng=77.2972601&restaurantId=${id}`)
-        const data = await res.json()
-        setResData(data)
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(()=>{
-         fetchData()
-    },[])
+  console.log(resData);
 
-    console.log(resData)
-
-  return (
-    <div>
-       Hii 
-    </div>
-  )
-}
+  return <div></div>;
+};
 
 export default RestaurantMenu;
